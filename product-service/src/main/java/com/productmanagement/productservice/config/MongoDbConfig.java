253a4da8e256ab324b14e2067cccbe888d5d55fb
Suresh MuthuSelvam <sureshkarthick37@gmail.com>
2023-02-14 21:23:37 +0530

    product service: mongo _class removal config

---
+++ product-service/src/main/java/com/productmanagement/productservice/config/MongoDbConfig.java
@@ -0,0 +1,18 @@
+package com.productmanagement.productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+@Configuration
+public class MongoDbConfig {
+
+	@Bean
+     MongoTemplate mongoTemplate(MongoDatabaseFactory databaseFactory, MappingMongoConverter converter) {
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return new MongoTemplate(databaseFactory, converter);
+    }
+}
