233355df76249fb170ff66c553306191f00f7385
Suresh MuthuSelvam <sureshkarthick37@gmail.com>
2023-02-14 07:04:12 +0000

    updated ProductServiceApplication

--- product-service/src/main/java/com/productmanagement/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/productmanagement/productservice/ProductServiceApplication.java
@@ -1,13 +1,7 @@
 package com.productmanagement.productservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-
-import com.productmanagement.productservice.model.Product;
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
@@ -16,9 +10,9 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 		
-		Product p = new Product("1","Pen","Butterflow",BigDecimal.valueOf(15));
-		System.out.println("Product;Pen");
-		p.toString();
+		// Product p = new Product("1","Pen","Butterflow",BigDecimal.valueOf(15));
+		// System.out.println("Product;Pen");
+		// p.toString();
 	}
 
 }
