c3671c0d1a2420fe352bfb11eac29860bb663b63
Suresh MuthuSelvam <sureshkarthick37@gmail.com>
2023-02-17 19:33:25 +0530

    removed old buggy eureka-server and created new one

--- discovery-server/src/test/java/com/productmanagement/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/productmanagement/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.productmanagement.eurekaserver;
+package com.productmanagement.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class DiscoveryServerApplicationTests {
 
 	@Test
 	void contextLoads() {
