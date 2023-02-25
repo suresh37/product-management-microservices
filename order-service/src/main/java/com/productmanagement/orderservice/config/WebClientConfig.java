adb7e093090885c31778c2c170586eb84977f357
Suresh MuthuSelvam <sureshkarthick37@gmail.com>
2023-02-25 22:40:41 +0530

    config package renamed

--- order-service/src/main/java/com/productmanagement/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/productmanagement/orderservice/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.productmanagement.orderservice.cconfig;
+package com.productmanagement.orderservice.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
